53be0563be1c5fc48e9c6420fb12222c877586fa
Nathan Chandler-Smith <nate@Nathans-MBP.mediacom.info>
2020-11-08 16:11:48 -0600

    refactors tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,9 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    private final String COMMON_ITEM = "common item";
+
     @Test
     void commonItem_whenUpdate_thenDecreaseQualityBy1() {
-        Item[] items = new Item[] { new Item("commonItem", 5, 10) };
+        Item[] items = new Item[] { new Item(COMMON_ITEM, 5, 10) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(9, app.items[0].quality);
@@ -16,10 +18,9 @@ class GildedRoseTest {
 
     @Test
     void givenCommonItem_whenUpdate_thenDecreaseSellInBy1() {
-        Item[] items = new Item[] { new Item("commonItem", 5, 10)};
+        Item[] items = new Item[] { new Item(COMMON_ITEM, 5, 10)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(4, app.items[0].sellIn);
     }
-
 }
