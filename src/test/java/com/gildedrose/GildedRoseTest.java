b7829435c2d370d727e0cd2726fefdd3039f42c4
Nathan Chandler-Smith <nate@Nathans-MBP.mediacom.info>
2020-11-08 17:00:10 -0600

    tests quality at 0 when update

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -39,12 +39,42 @@ class GildedRoseTest {
     }
 
     @Test
-    void givenCommonItem_whenUpdateWithSellinEquals0_thenDecrementSellIn() {
+    void givenCommonItem_whenUpdateWithSellInEquals0_thenDecrementSellIn() {
         Item[] items = new Item[] { new Item(COMMON_ITEM, 0, 10 )};
         GildedRose app = new GildedRose(items);
 
         app.updateQuality();
 
         assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    void givenCommonItemWithNegativeSellIn_whenUpdate_thenDecrementSellin() {
+        Item[] items = new Item[] { new Item(COMMON_ITEM, 0-1, 10 )};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(-2, app.items[0].sellIn);
+    }
+
+    @Test
+    void givenCommonItemWithNegativeSellIn_whenUpdate_thenDecreaseQualityBy2() {
+        Item[] items = new Item[] { new Item(COMMON_ITEM, -2, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    void givenCommonItemWith0Quality_whenUpdate_thenQualityRemains0() {
+        Item[] items = new Item[] { new Item(COMMON_ITEM, 5, 0) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
     }
 }
