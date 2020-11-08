a546538a6936d9dc39b109e5601bb3cfc8eb6308
Nathan Chandler-Smith <nate@Nathans-MBP.mediacom.info>
2020-11-08 15:22:42 -0600

    update decreases sellin by 1

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,12 @@ class GildedRoseTest {
         assertEquals(9, app.items[0].quality);
     }
 
+    @Test
+    void givenCommonItem_whenUpdate_thenDecreaseSellInBy1() {
+        Item[] items = new Item[] { new Item("commonItem", 5, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4, app.items[0].sellIn);
+    }
+
 }
