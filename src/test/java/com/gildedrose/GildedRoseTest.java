ede3e5b756e19ce019260677b425ed5b67037b42
Nathan Chandler-Smith <nate@Nathans-MBP.mediacom.info>
2020-11-08 16:18:26 -0600

    refactors

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,15 +12,29 @@ class GildedRoseTest {
     void commonItem_whenUpdate_thenDecreaseQualityBy1() {
         Item[] items = new Item[] { new Item(COMMON_ITEM, 5, 10) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals(9, app.items[0].quality);
     }
 
     @Test
     void givenCommonItem_whenUpdate_thenDecreaseSellInBy1() {
-        Item[] items = new Item[] { new Item(COMMON_ITEM, 5, 10)};
+        Item[] items = new Item[] { new Item(COMMON_ITEM, 5, 10) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+        
         assertEquals(4, app.items[0].sellIn);
+    }
+
+    @Test
+    void givenCommonItem_whenUpdateWithSellInEquals0_thenDecreaseQualityBy2() {
+        Item[] items = new Item[] { new Item(COMMON_ITEM, 0, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        
+        assertEquals(8, app.items[0].quality);
     }
 }
