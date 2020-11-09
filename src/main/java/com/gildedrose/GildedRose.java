f5cecd077284b6dc7426f96d8631463df30de74e
Nathan Chandler-Smith <nate@Nathans-MBP.mediacom.info>
2020-11-08 21:43:30 -0600

    refactors

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,52 +13,77 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE)
-                    && !items[i].name.equals(PASS)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS)) {
-                        items[i].quality = items[i].quality - 1;
+            updateWhenNotExpired(i);
+            updateSellIn(i);
+            updateQualityDueToExperation(i);
+        }
+    }
+
+    private void updateWhenNotExpired(int i) {
+        if (itemIsCommon(i)) {
+            updateQualityCommonItem(i);
+        } else {
+            updateQualityUncommonItem(i);
+        }
+    }
+
+    private boolean itemIsCommon(int i) {
+        return !items[i].name.equals(AGED_BRIE)
+             && !items[i].name.equals(PASS)
+             && !items[i].name.equals(SULFURAS);
+    }
+
+    private void updateQualityCommonItem(int i) {
+        if (items[i].quality > 0) {
+            items[i].quality -= 1;
+        }
+    }
+
+    private void updateQualityUncommonItem(int i) {
+        final Integer maxQuality = 50;
+
+        if (items[i].quality < maxQuality) {
+            items[i].quality += 1;
+            if (items[i].name.equals(PASS)) {
+                additionalPassQualityUpdates(i, maxQuality);
+            }
+        }
+    }
+
+    private void additionalPassQualityUpdates(int i, final Integer maxQuality) {
+        if (items[i].sellIn < 11) {
+            if (items[i].quality < maxQuality) {
+                items[i].quality += 1;
+            }
+        }
+        if (items[i].sellIn < 6) {
+            if (items[i].quality < maxQuality) {
+                items[i].quality += 1;
+            }
+        }
+    }
+
+    private void updateSellIn(int i) {
+        if (!items[i].name.equals(SULFURAS)) {
+            items[i].sellIn -= 1;
+        }
+    }
+
+    private void updateQualityDueToExperation(int i) {
+        if (items[i].sellIn < 0) {
+            if (!items[i].name.equals(AGED_BRIE)) {
+                if (!items[i].name.equals(PASS)) {
+                    if (items[i].quality > 0) {
+                        if (!items[i].name.equals(SULFURAS)) {
+                            items[i].quality -= 1;
+                        }
                     }
+                } else {
+                    items[i].quality = 0;
                 }
             } else {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals(PASS)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals(SULFURAS)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE)) {
-                    if (!items[i].name.equals(PASS)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    items[i].quality += 1;
                 }
             }
         }
