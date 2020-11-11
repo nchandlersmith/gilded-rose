81c35bd95140a52792c3f084716db905b877cee4
Nathan Chandler-Smith <nate@Nathans-MBP.mediacom.info>
2020-11-10 21:16:24 -0600

    removes dead code

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -15,7 +15,6 @@ class GildedRose {
             if (items[i] instanceof StoreItem) {
                 ((StoreItem) items[i]).updateQuality();
             }
-            new Exception("some exception").toString();
         }
     }
 }
