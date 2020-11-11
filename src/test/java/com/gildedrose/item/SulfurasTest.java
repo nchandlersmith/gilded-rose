7f06ecb79c43a1663c49ac7f909f117f24a5002c
Nathan Chandler-Smith <nate@Nathans-MBP.mediacom.info>
2020-11-10 21:10:50 -0600

    refactors to parameterized tests

--- src/test/java/com/gildedrose/item/SulfurasTest.java
+++ src/test/java/com/gildedrose/item/SulfurasTest.java
@@ -3,33 +3,18 @@ package com.gildedrose.item;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SulfurasTest {
     private  final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     
 
 
-    @Test
-    void givenSulfurasWithPositiveSellIn_whenUpdate_thenQualityDoesNotDecrease() {
-        Sulfuras sulfuras = new Sulfuras(SULFURAS, 5, 80);
-
-        sulfuras.updateQuality();
-
-        assertEquals(80, sulfuras.quality);
-    }
-
-    @Test
-    void givenSulfurasWithSellinEquals0_whenUpdate_thenQualityDoesNotDecrease() {
-        Sulfuras sulfuras = new Sulfuras(SULFURAS, 0, 80);
-
-        sulfuras.updateQuality();
-
-        assertEquals(80, sulfuras.quality);
-    }
-
-    @Test
-    void givenSulfurasWithNegativeSellIn_whenUpdate_thenQualityDoesNotDecrease() {
-        Sulfuras sulfuras = new Sulfuras(SULFURAS, -1, 80);
+    @ParameterizedTest
+    @ValueSource(ints = {1, 0, -1})
+    void givenSulfurasWithPositiveSellIn_whenUpdate_thenQualityDoesNotDecrease(Integer sellIn) {
+        Sulfuras sulfuras = new Sulfuras(SULFURAS, sellIn, 80);
 
         sulfuras.updateQuality();
 
