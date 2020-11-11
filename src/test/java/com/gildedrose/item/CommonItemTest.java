5f60fc33b087cabb6ca5a4d2aacd17e04d86377d
Nathan Chandler-Smith <nate@Nathans-MBP.mediacom.info>
2020-11-10 18:17:38 -0600

    adds more unit tests for common item

--- src/test/java/com/gildedrose/item/CommonItemTest.java
+++ src/test/java/com/gildedrose/item/CommonItemTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 
 public class CommonItemTest {
 
+    private final String COMMON_ITEM = "common item";
+
     @Test
     void update_whenSellInGreaterThan0_thenDecreaseQualityBy1() {
-        CommonItem commonItem = new CommonItem("common item", 1, 10);
+        CommonItem commonItem = new CommonItem(COMMON_ITEM, 1, 10);
         
         commonItem.updateQuality();
 
@@ -17,7 +19,7 @@ public class CommonItemTest {
 
     @Test
     void update_whenSellInGreaterThan0_thenDecreaseSellInBy1() {
-        CommonItem commonItem = new CommonItem("common item", 1, 10);
+        CommonItem commonItem = new CommonItem(COMMON_ITEM, 1, 10);
 
         commonItem.updateQuality();
 
@@ -26,10 +28,46 @@ public class CommonItemTest {
 
     @Test
     void update_whenSellInEquals0_thenDecreaseQualityBy2() {
-        CommonItem commonItem = new CommonItem("common item", 0, 10);
+        CommonItem commonItem = new CommonItem(COMMON_ITEM, 0, 10);
 
         commonItem.updateQuality();
 
         assertEquals(8, commonItem.quality);
+    }
+
+    @Test
+    void givenCommonItem_whenUpdateWithSellInEquals0_thenDecrementSellIn() {
+        CommonItem commonItem = new CommonItem(COMMON_ITEM, 0, 10);
+
+        commonItem.updateQuality();
+
+        assertEquals(-1, commonItem.sellIn);
+    }
+
+    @Test
+    void givenCommonItemWithNegativeSellIn_whenUpdate_thenDecrementSellin() {
+        CommonItem commonItem = new CommonItem(COMMON_ITEM, -1, 10);
+
+        commonItem.updateQuality();
+
+        assertEquals(-2, commonItem.sellIn);
+    }
+
+    @Test
+    void givenCommonItemWithNegativeSellIn_whenUpdate_thenDecreaseQualityBy2() {
+        CommonItem commonItem = new CommonItem(COMMON_ITEM, -1, 10);
+
+        commonItem.updateQuality();
+
+        assertEquals(8, commonItem.quality);
+    }
+
+    @Test
+    void givenCommonItemWith0QualityAnd0SellIn_whenUpdate_thenQualityRemains0() {
+        CommonItem commonItem = new CommonItem(COMMON_ITEM, 0, 0);
+
+        commonItem.updateQuality();
+
+        assertEquals(0, commonItem.quality);
     }
 }
