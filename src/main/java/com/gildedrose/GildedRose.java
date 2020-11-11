e202e6c111a5884fe2abf5e7947c6f98ba9b911f
Nathan Chandler-Smith <nate@Nathans-MBP.mediacom.info>
2020-11-10 18:50:15 -0600

    deletes dead code

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,11 +3,6 @@ package com.gildedrose;
 import com.gildedrose.item.StoreItem;
 
 class GildedRose {
-    private static final String AGED_BRIE = "Aged Brie";
-    private static final String PASS = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    private static final Integer MAXIMUM_QUALITY = 50;
-    private static final Integer MINIMUM_QUALITY = 0;
 
     Item[] items;
 
@@ -19,87 +14,7 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (items[i] instanceof StoreItem) {
                 ((StoreItem) items[i]).updateQuality();
-                break;
             }
-            updateQualityWhenNotExpired(i);
-            updateSellInIfNotSulfuras(i);
-            updateQualityDueToExpiration(i);
-        }
-    }
-
-    private void updateQualityWhenNotExpired(int i) {
-        updateUnexpiredQualityIfCommonItem(i);
-        updateQualityIfAgedBrie(i);
-        updateUnexpiredQualityIfPass(i);
-    }
-
-    private void updateUnexpiredQualityIfCommonItem(int i) {
-        updateQualityDueToExpirationIfCommonItem(i);
-    }
-
-    private boolean itemIsCommon(int i) {
-        return !items[i].name.equals(AGED_BRIE)
-             && !items[i].name.equals(PASS)
-             && !items[i].name.equals(SULFURAS);
-    }
-
-    private void updateUnexpiredQualityIfPass(int i) {
-        if (items[i].name.equals(PASS)) {
-            incrementItemQuality(i);
-            additionalPassQualityUpdates(i);
-        }
-    }
-
-    private void additionalPassQualityUpdates(int i) {
-        if (items[i].sellIn < 11) {
-            incrementItemQuality(i);
-        }
-        if (items[i].sellIn < 6) {
-            incrementItemQuality(i);
-        }
-    }
-
-    private void updateSellInIfNotSulfuras(int i) {
-        if (!items[i].name.equals(SULFURAS)) {
-            items[i].sellIn -= 1;
-        }
-    }
-
-    private void updateQualityDueToExpiration(int i) {
-        if (items[i].sellIn < 0) {
-            updateQualityDueToExpirationIfCommonItem(i);
-            updateQualityDueToExpirationIfPass(i);
-            updateQualityIfAgedBrie(i);
-        }
-    }
-
-    private void updateQualityDueToExpirationIfCommonItem(int i) {
-        if (itemIsCommon(i)) {
-            decrementItemQuality(i);
-        }
-    }
-
-    private void updateQualityDueToExpirationIfPass(int i) {
-        if (items[i].name.equals(PASS)) {
-            items[i].quality = 0;
-        }
-    }
-
-    private void updateQualityIfAgedBrie(int i) {
-        if (items[i].name.equals(AGED_BRIE)) {
-            incrementItemQuality(i);
-        }
-    }
-
-    private void decrementItemQuality(int i) {
-        if (items[i].quality > MINIMUM_QUALITY) {
-            items[i].quality -= 1;
-        }
-    }
-
-    private void incrementItemQuality(int i) {
-        if (items[i].quality < MAXIMUM_QUALITY) {
-            items[i].quality += 1;
         }
     }
 }
