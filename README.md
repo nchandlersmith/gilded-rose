1a77c294fcca49a5b84579d3474354f8c96db4cd
Nathan Chandler-Smith <nate@Nathans-MBP.mediacom.info>
2020-11-14 08:51:28 -0600

    refactors aged brie

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gilded-rose
 https://github.com/emilybache/GildedRose-Refactoring-Kata/tree/master/Java
+
+## Notes
+ -It is tempting to make the Item class into an interface, then implement that interface. This kata does not allow this. See section about goblin.
--- src/main/java/com/gildedrose/item/AgedBrie.java
+++ src/main/java/com/gildedrose/item/AgedBrie.java
@@ -2,17 +2,35 @@ package com.gildedrose.item;
 
 public class AgedBrie extends StoreItem {
 
+    private static final Integer MAXIMUM_QUALITY = 50;
+
     public AgedBrie(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
 
     @Override
     public void updateQuality() {
-        if (quality < 50) quality += 1;
+        incrementQualityWithinMaximum();
+        decrementSellIn();
+        incrementWhenExpired();
+    }
+
+    private void incrementQualityWithinMaximum() {
+        if (quality < MAXIMUM_QUALITY) quality += 1;
+    }
+
+    private void decrementSellIn() {
         sellIn -= 1;
-        if (sellIn < 0 && quality < 50) {
-            quality += 1;
+    }
+
+    private void incrementWhenExpired() {
+        if (hasExpired()) {
+            incrementQualityWithinMaximum();
         }
+    }
+
+    private boolean hasExpired() {
+        return sellIn < 0;
     }
     
 }
