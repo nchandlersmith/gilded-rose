598bb9b9e061e3a5bc302e9ef279281289aeb2c5
Nathan Chandler-Smith <nate@Nathans-MBP.mediacom.info>
2020-11-14 09:27:46 -0600

    refactors

--- src/main/java/com/gildedrose/item/CommonItem.java
+++ src/main/java/com/gildedrose/item/CommonItem.java
@@ -2,21 +2,35 @@ package com.gildedrose.item;
 
 public class CommonItem extends StoreItem {
 
+    private static final Integer MINIMUM_QUALITY = 0;
+
     public CommonItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
 
     @Override
 	public void updateQuality() {
-        safeDecrementQuality();
-        sellIn -= 1;
-        if (sellIn < 0) {
-            safeDecrementQuality();
-        }
+        decrementQualityWithinMaximum();
+        decrementSellIn();
+        decrementWhenExpired();
     }
     
-    private void safeDecrementQuality() {
-        if (quality > 0) quality -= 1;
+    private void decrementQualityWithinMaximum() {
+        if (quality > MINIMUM_QUALITY) quality -= 1;
+    }
+
+    private void decrementSellIn() {
+        sellIn -= 1;
+    }
+
+    private void decrementWhenExpired() {
+        if (hasExpired()) {
+            decrementQualityWithinMaximum();
+        }
+    }
+
+    private boolean hasExpired() {
+        return sellIn < 0;
     }
     
 }
