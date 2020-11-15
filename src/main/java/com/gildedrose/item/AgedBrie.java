9cc09e4a4b83df9d92876565315e4b37ea84a679
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2020-11-15 09:19:42 -0600

    Aged Brie uses Store Item to update quality

--- src/main/java/com/gildedrose/item/AgedBrie.java
+++ src/main/java/com/gildedrose/item/AgedBrie.java
@@ -2,17 +2,9 @@ package com.gildedrose.item;
 
 public class AgedBrie extends StoreItem {
 
-    private static final int MAXIMUM_QUALITY = 50;
-
     public AgedBrie(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
-    }
-
-    @Override
-    public void updateQuality() {
-        sellIn -= 1;
-        quality += sellIn < 0 ? 2 : 1;
-        quality = quality > MAXIMUM_QUALITY ? MAXIMUM_QUALITY : quality;
+        super.setQualityIncrement(1);
     }
     
 }
--- src/main/java/com/gildedrose/item/StoreItem.java
+++ src/main/java/com/gildedrose/item/StoreItem.java
@@ -7,7 +7,7 @@ public abstract class StoreItem extends Item{
     private int qualityIncrement = -1;
 
     private static final int MINIMUM_QUALITY = 0;
-
+    private static final int MAXIMUM_QUALITY = 50;
 
     protected StoreItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
@@ -17,6 +17,7 @@ public abstract class StoreItem extends Item{
         sellIn -= 1;
         quality += sellIn < 0 ? 2 * qualityIncrement : qualityIncrement;
         quality = quality < MINIMUM_QUALITY ? MINIMUM_QUALITY : quality;
+        quality = quality > MAXIMUM_QUALITY ? MAXIMUM_QUALITY  : quality;
     }
 
     protected void setQualityIncrement(int qualityIncrement) {
