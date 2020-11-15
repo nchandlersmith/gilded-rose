f356ba7bf11a4e5755fa51c79aaa3a6b9a0e5914
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2020-11-15 09:16:00 -0600

    CommonItem uses StoreItem to update quality

--- src/main/java/com/gildedrose/item/CommonItem.java
+++ src/main/java/com/gildedrose/item/CommonItem.java
@@ -2,17 +2,8 @@ package com.gildedrose.item;
 
 public class CommonItem extends StoreItem {
 
-    private static final int MINIMUM_QUALITY = 0;
-
     public CommonItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
-    }
-
-    @Override
-	public void updateQuality() {
-        sellIn -= 1;
-        quality -= sellIn < 0 ? 2 : 1;
-        quality = quality < MINIMUM_QUALITY ? MINIMUM_QUALITY : quality;
     }
     
 }
--- src/main/java/com/gildedrose/item/StoreItem.java
+++ src/main/java/com/gildedrose/item/StoreItem.java
@@ -4,10 +4,23 @@ import com.gildedrose.Item;
 
 public abstract class StoreItem extends Item{
 
+    private int qualityIncrement = -1;
+
+    private static final int MINIMUM_QUALITY = 0;
+
+
     protected StoreItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
 
-    public abstract void updateQuality();
+    public void updateQuality(){
+        sellIn -= 1;
+        quality += sellIn < 0 ? 2 * qualityIncrement : qualityIncrement;
+        quality = quality < MINIMUM_QUALITY ? MINIMUM_QUALITY : quality;
+    }
+
+    protected void setQualityIncrement(int qualityIncrement) {
+        this.qualityIncrement = qualityIncrement;
+    }
     
 }
