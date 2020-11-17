4d1f2161cad29dd5d7a9fc72853c85dc5a947aa4
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2020-11-17 15:47:53 -0600

    parameterizes test

--- src/test/java/com/gildedrose/rule/RuleQualityImprovesTieredTest.java
+++ src/test/java/com/gildedrose/rule/RuleQualityImprovesTieredTest.java
@@ -1,6 +1,5 @@
 package com.gildedrose.rule;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -19,11 +18,23 @@ class RuleQualityImprovesTieredTest {
         assertEquals(expectedQuality, actualQuality);
     }
 
-    static Stream<Arguments> tieredRuleSpecs() {
+    private static Stream<Arguments> tieredRuleSpecs() {
         return Stream.of(
-                Arguments.arguments(11, 21, 22), // sellIn > 10
-                Arguments.arguments(10, 21, 23), // 5 < sellIn < 11
-                Arguments.arguments(5, 21, 24) // -1 < sellIn < 6
+                whenSellinGreaterThan10_thenIncreaseQualityBy1(),
+                whenSellingBetween5And11IExclusive_thenIncreaseQualityBy2(),
+                whenSellInNotNegativeAndLessThan6Exclusive_thenIncreaseQualityBy3()
         );
+    }
+
+    private static Arguments whenSellinGreaterThan10_thenIncreaseQualityBy1() {
+        return Arguments.arguments(11, 21, 22);
+    }
+
+    private static Arguments whenSellingBetween5And11IExclusive_thenIncreaseQualityBy2() {
+        return Arguments.arguments(10, 21, 23);
+    }
+
+    private static Arguments whenSellInNotNegativeAndLessThan6Exclusive_thenIncreaseQualityBy3() {
+        return Arguments.arguments(5, 21, 24);
     }
 }
