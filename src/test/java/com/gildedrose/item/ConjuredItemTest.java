7af63077fe75b4dff7284d91f886efff0f681c74
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2020-11-18 19:04:39 -0600

    tests use AssertJ and are parameterized

--- src/test/java/com/gildedrose/item/ConjuredItemTest.java
+++ src/test/java/com/gildedrose/item/ConjuredItemTest.java
@@ -1,99 +1,92 @@
 package com.gildedrose.item;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class ConjuredItemTest {
+import java.util.stream.Stream;
+
+class ConjuredItemTest {
     private final String CONJURED_ITEM = "conjured item";
 
     @Test
     void update_whenSellInGreaterThan0_thenDecreaseQualityBy2() {
-        ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, 1, 10);
-        
+        StoreItem conjuredItem = StoreItemFactory.createConjuredItem(CONJURED_ITEM, 1,10);
         conjuredItem.updateQuality();
-
-        assertEquals(8, conjuredItem.quality);
+        assertThat(conjuredItem.quality).isEqualTo(8);
     }
     
     @Test
-    void update_whenSellInGreaterThan0_thenDecreaseSellinBy1() {
+    void update_whenSellInGreaterThan0_thenDecreaseSellInBy1() {
         ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, 1, 10);
-        
         conjuredItem.updateQuality();
-
-        assertEquals(0, conjuredItem.sellIn);
+        assertThat(conjuredItem.sellIn).isZero();
     }
-    
     @Test
     void update_whenSellInEquals0_thenDecreaseQualityBy4() {
         ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, 0, 10);
-        
         conjuredItem.updateQuality();
-
-        assertEquals(6, conjuredItem.quality);
+        assertThat(conjuredItem.quality).isEqualTo(6);
     }
     
     @Test
-    void update_whenSellInEquals0_thenDecreaseSellinBy1() {
+    void update_whenSellInEquals0_thenDecreaseSellInBy1() {
         ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, 0, 10);
-        
         conjuredItem.updateQuality();
-
-        assertEquals(-1, conjuredItem.sellIn);
+        assertThat(conjuredItem.sellIn).isEqualTo(-1);
     }
     
     @Test
-    void update_whenSellInNegative_thenDecreaseSellinBy1() {
+    void update_whenSellInNegative_thenDecreaseSellInBy1() {
         ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, -1, 10);
-        
         conjuredItem.updateQuality();
+        assertThat(conjuredItem.sellIn).isEqualTo(-2);
+    }
 
-        assertEquals(-2, conjuredItem.sellIn);
+    @Test
+    void givenCommonItemWith3QualityAnd1SellIn_whenUpdate_thenQualityEquals1() {
+        ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, 1, 3);
+        conjuredItem.updateQuality();
+        assertThat(conjuredItem.quality).isEqualTo(1);
     }
     
     @Test
     void update_whenSellInNegative_thenDecreaseQualityBy4() {
-        ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, -1, 10);
-        
+        ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, -1, 4);
         conjuredItem.updateQuality();
-
-        assertEquals(6, conjuredItem.quality);
+        assertThat(conjuredItem.quality).isZero();
     }
-    
-    @Test
-    void givenCommonItemWith0QualityAnd0SellIn_whenUpdate_thenQualityRemains0() {
-        ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, 0, 0);
-        
+
+    private static Arguments sellInEquals1_startingQualityEquals1_finalQualityEquals0() {
+        return Arguments.arguments(1,1);
+    }
+
+    private static Arguments sellInEquals0_startingQualityEquals3_finalQualityEquals0() {
+        return Arguments.arguments(0, 3);
+    }
+
+    private static Arguments sellInEquals0_startingQualityEquals0_finalQualityEquals0() {
+        return Arguments.arguments(0, 0);
+    }
+
+    @ParameterizedTest(name = "#{index} - sellin: {0} starting quality: {1} final quality equals 0")
+    @MethodSource("generateArgumentsForTests")
+    void paramTests(int sellIn, int quality) {
+        ConjuredItem conjuredItem = new ConjuredItem("some conjured item", sellIn, quality);
         conjuredItem.updateQuality();
+        assertThat(conjuredItem.quality).isZero();
 
-        assertEquals(0, conjuredItem.quality);
     }
-    
-    @Test
-    void givenCommonItemWith1QualityAnd1SellIn_whenUpdate_thenQualityEquals0() {
-        ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, 1, 1);
-        
-        conjuredItem.updateQuality();
 
-        assertEquals(0, conjuredItem.quality);
+    private static Stream<Arguments> generateArgumentsForTests() {
+        return Stream.of(
+                sellInEquals1_startingQualityEquals1_finalQualityEquals0(),
+                sellInEquals0_startingQualityEquals3_finalQualityEquals0(),
+                sellInEquals0_startingQualityEquals0_finalQualityEquals0()
+        );
     }
-    
-    @Test
-    void givenCommonItemWith3QualityAnd0SellIn_whenUpdate_thenQualityEquals0() {
-        ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, 0, 3);
-        
-        conjuredItem.updateQuality();
 
-        assertEquals(0, conjuredItem.quality);
-    }
-    
-    @Test
-    void givenCommonItemWith3QualityAnd1SellIn_whenUpdate_thenQualityEquals1() {
-        ConjuredItem conjuredItem = new ConjuredItem(CONJURED_ITEM, 1, 3);
-        
-        conjuredItem.updateQuality();
-
-        assertEquals(1, conjuredItem.quality);
-    }
 }
