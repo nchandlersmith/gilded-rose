dd18c7a39417a0067083c6577475ed481019f4b4
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2020-11-21 10:49:45 -0600

    improves store item equality testing

--- src/test/java/com/gildedrose/item/CommonItemTest.java
+++ src/test/java/com/gildedrose/item/CommonItemTest.java
@@ -12,99 +12,50 @@ class CommonItemTest {
 
     @Test
     void update_whenSellInGreaterThan0_thenDecreaseQualityBy1() {
-        StoreItem commonItem = new StoreItem.Builder()
-                                        .name(COMMON_ITEM)
-                                        .sellIn(1)
-                                        .quality(10)
-                                        .ruleCalculateQuality(new RuleCalculateQualityDegrades(1))
-                                        .build();
-
+        StoreItem commonItem = StoreItemFactory.createCommonItem(COMMON_ITEM, 1, 10);
         commonItem.updateQuality();
-
         assertEquals(9, commonItem.quality);
     }
 
     @Test
     void update_whenSellInGreaterThan0_thenDecreaseSellInBy1() {
-        StoreItem commonItem = new StoreItem.Builder()
-                                        .name(COMMON_ITEM)
-                                        .sellIn(1)
-                                        .quality(10)
-                                        .ruleCalculateQuality(new RuleCalculateQualityDegrades(1))
-                                        .build();
-
+        StoreItem commonItem = StoreItemFactory.createCommonItem(COMMON_ITEM, 1, 10);
         commonItem.updateQuality();
-
         assertEquals(0, commonItem.sellIn);
     }
 
     @Test
     void update_whenSellInEquals0_thenDecreaseQualityBy2() {
-        StoreItem commonItem = new StoreItem.Builder()
-                                        .name(COMMON_ITEM)
-                                        .sellIn(0)
-                                        .quality(10)
-                                        .ruleCalculateQuality(new RuleCalculateQualityDegrades(1))
-                                        .build();
-
+        StoreItem commonItem = StoreItemFactory.createCommonItem(COMMON_ITEM, 0, 10);
         commonItem.updateQuality();
-
         assertEquals(8, commonItem.quality);
     }
 
     @Test
     void givenCommonItem_whenUpdateWithSellInEquals0_thenDecrementSellIn() {
-        StoreItem commonItem = new StoreItem.Builder()
-                                        .name(COMMON_ITEM)
-                                        .sellIn(0)
-                                        .quality(10)
-                                        .ruleCalculateQuality(new RuleCalculateQualityDegrades(1))
-                                        .build();
-
+        StoreItem commonItem = StoreItemFactory.createCommonItem(COMMON_ITEM, 0, 10);
         commonItem.updateQuality();
-
         assertEquals(-1, commonItem.sellIn);
     }
 
     @Test
     void givenCommonItemWithNegativeSellIn_whenUpdate_thenDecrementSellin() {
-        StoreItem commonItem = new StoreItem.Builder()
-                                        .name(COMMON_ITEM)
-                                        .sellIn(-1)
-                                        .quality(10)
-                                        .ruleCalculateQuality(new RuleCalculateQualityDegrades(1))
-                                        .build();
-
+        StoreItem commonItem = StoreItemFactory.createCommonItem(COMMON_ITEM, -1, 10);
         commonItem.updateQuality();
-
         assertEquals(-2, commonItem.sellIn);
     }
 
     @Test
     void givenCommonItemWithNegativeSellIn_whenUpdate_thenDecreaseQualityBy2() {
-        StoreItem commonItem = new StoreItem.Builder()
-                                        .name(COMMON_ITEM)
-                                        .sellIn(-1)
-                                        .quality(10)
-                                        .ruleCalculateQuality(new RuleCalculateQualityDegrades(1))
-                                        .build();
-
+        StoreItem commonItem = StoreItemFactory.createCommonItem(COMMON_ITEM, -1, 10);
         commonItem.updateQuality();
-
         assertEquals(8, commonItem.quality);
     }
 
     @Test
-    void givenCommonItemWith0QualityAnd0SellIn_whenUpdate_thenQualityRemains0() {
-        StoreItem commonItem = new StoreItem.Builder()
-                                        .name(COMMON_ITEM)
-                                        .sellIn(0)
-                                        .quality(0)
-                                        .ruleCalculateQuality(new RuleCalculateQualityDegrades(1))
-                                        .build();
-
+    void givenCommonItem_qualityCannotBNegative() {
+        StoreItem commonItem = StoreItemFactory.createCommonItem(COMMON_ITEM, -1, 0);
         commonItem.updateQuality();
-
         assertEquals(0, commonItem.quality);
     }
 }
