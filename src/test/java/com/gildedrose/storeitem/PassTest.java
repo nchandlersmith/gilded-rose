bc1e17b0eb1eefd3cf56ec6fd55e4a76c53c50d1
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2020-11-24 22:23:51 -0600

    refactors tests

--- src/test/java/com/gildedrose/storeitem/PassTest.java
+++ src/test/java/com/gildedrose/storeitem/PassTest.java
@@ -1,94 +1,126 @@
 package com.gildedrose.storeitem;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class PassTest {
 
     private final String PASSES = "Backstage passes to a TAFKAL80ETC concert";
 
     @Test
-    void givenPassesWithSellInGreaterThan10_whenUpdate_thenIncreaseQualityBy1() {
+    void updateQuality_whenInGreaterThan10_thenIncreaseQualityBy1() {
         StoreItem pass = StoreItemFactory.createPass(PASSES, 11, 20);
         pass.updateQuality();
-        assertEquals(21, pass.quality);
+        assertThat(pass.quality).isEqualTo(21);
     }
-    
-    @Test
-    void givenPassesWithSellInEquals10_whenUpdate_thenIncreaseQualityBy2() {
-        StoreItem pass = StoreItemFactory.createPass(PASSES, 10, 20);
+
+    @ParameterizedTest(name = "#{index} - sellIn: {0} startingQuality: {1} expectedQuality: {2}")
+    @MethodSource("createArguments_whenSellIn6To10Inclusive_increasesQualityBy2")
+    void updateQuality_whenSellIn6to10Inclusive_increasesQualityBy2(int sellIn, int startingQuality, int expectedQuality) {
+        StoreItem pass = StoreItemFactory.createPass(PASSES, sellIn, startingQuality);
         pass.updateQuality();
-        assertEquals(22, pass.quality);
+        assertThat(pass.quality).isEqualTo(expectedQuality);
     }
-    
-    @Test
-    void givenPassesWithSellInAtOrBetween6And10_whenUpdate_thenIncreaseQualityBy2() {
-        StoreItem pass = StoreItemFactory.createPass(PASSES, 6, 20);
-        pass.updateQuality();
-        assertEquals(22, pass.quality);
+
+    private static Stream<Arguments> createArguments_whenSellIn6To10Inclusive_increasesQualityBy2() {
+        return Stream.of(
+                // sellIn, startingQuality, expectedQuality
+                Arguments.arguments(10, 10, 12),
+                Arguments.arguments(9, 10, 12),
+                Arguments.arguments(8, 10, 12),
+                Arguments.arguments(7, 10, 12),
+                Arguments.arguments(6, 10, 12)
+        );
     }
-    
-    @Test
-    void givenPassesWithSellInEquals5_whenUpdate_thenIncreaseQualityBy3() {
-        StoreItem pass = StoreItemFactory.createPass(PASSES, 5, 20);
+
+    @ParameterizedTest(name = "#{index} - sellIn: {0} startingQuality: {1} expectedQuality: {2}")
+    @MethodSource("createArguments_whenSellIn1To5Inclusive_increasesQualityBy3")
+    void updateQuality_whenSellIn1To5Inclusive_increasesQualityBy3(int sellIn, int startingQuality, int expectedQuality) {
+        StoreItem pass = StoreItemFactory.createPass(PASSES, sellIn, startingQuality);
         pass.updateQuality();
-        assertEquals(23, pass.quality);
+        assertThat(pass.quality).isEqualTo(expectedQuality);
     }
-    
-    @Test
-    void givenPassesWithSellInAtOrBetween1And5_whenUpdate_thenIncreaseQualityBy3() {
-        StoreItem pass = StoreItemFactory.createPass(PASSES, 1, 20);
-        pass.updateQuality();
-        assertEquals(23, pass.quality);
+
+    private static Stream<Arguments> createArguments_whenSellIn1To5Inclusive_increasesQualityBy3() {
+        return Stream.of(
+                // sellIn, startingQuality, expectedQuality
+                Arguments.arguments(5, 10, 13),
+                Arguments.arguments(4, 10, 13),
+                Arguments.arguments(3, 10, 13),
+                Arguments.arguments(2, 10, 13),
+                Arguments.arguments(1, 10, 13)
+        );
     }
-    
-    @Test
-    void givenPassesWithSellInEquals0_whenUpdate_thenQualityEquals0() {
-        StoreItem pass = StoreItemFactory.createPass(PASSES, 0, 20);
+
+    @ParameterizedTest(name = "#{index} startingSellIn: {0} expectedSellIn: {1}")
+    @MethodSource("createArguments_sellInAlwaysDecrements")
+    void updateQuality_sellInAlwaysDecrements(int startingSellIn, int expectedSellIn) {
+        StoreItem pass = StoreItemFactory.createPass(PASSES, startingSellIn, 12);
         pass.updateQuality();
-        assertEquals(0, pass.quality);
+        assertThat(pass.sellIn).isEqualTo(expectedSellIn);
     }
-    
-    @Test
-    void givenPassesWithSellInLessThan0_whenUpdate_thenQualityEquals0() {
-        StoreItem pass = StoreItemFactory.createPass(PASSES, -1, 20);
-        pass.updateQuality();
-        assertEquals(0, pass.quality);
+
+    private static Stream<Arguments> createArguments_sellInAlwaysDecrements() {
+        return Stream.of(
+                // startingSellIn, expectedSellIn
+                Arguments.arguments(11, 10),
+                Arguments.arguments(10, 9),
+                Arguments.arguments(9, 8),
+                Arguments.arguments(8, 7),
+                Arguments.arguments(7, 6),
+                Arguments.arguments(6, 5),
+                Arguments.arguments(5, 4),
+                Arguments.arguments(4, 3),
+                Arguments.arguments(3, 2),
+                Arguments.arguments(2, 1),
+                Arguments.arguments(1, 0),
+                Arguments.arguments(0, -1),
+                Arguments.arguments(-1, -2)
+        );
     }
-    
-    @Test
-    void givenPassesWithSellInEquals0_whenUpdate_thenSellInDecrements() {
-        StoreItem pass = StoreItemFactory.createPass(PASSES, 0, 20);
+
+    @ParameterizedTest(name = "#{index} sellIn: {0} startingQuality: {1} expectedQuality: {2}")
+    @MethodSource("createArguments_maximumQualityIs50")
+    void updateQuality_maximumQualityIs50(int sellIn, int startingQuality, int expectedQuality) {
+        StoreItem pass = StoreItemFactory.createPass(PASSES, sellIn, startingQuality);
         pass.updateQuality();
-        assertEquals(-1, pass.sellIn);
+        assertThat(pass.quality).isEqualTo(expectedQuality);
     }
-    
-    @Test
-    void givenPassesWithSellInEqualsNegative1_whenUpdate_thenSellInDecrements() {
-        StoreItem pass = StoreItemFactory.createPass(PASSES, -1, 20);
-        pass.updateQuality();
-        assertEquals(-2, pass.sellIn);
+
+    private static Stream<Arguments> createArguments_maximumQualityIs50() {
+        return Stream.of(
+                Arguments.arguments(11 ,50, 50),
+                Arguments.arguments(10 ,50, 50),
+                Arguments.arguments(9 ,50, 50),
+                Arguments.arguments(8 ,50, 50),
+                Arguments.arguments(7 ,50, 50),
+                Arguments.arguments(6 ,50, 50),
+                Arguments.arguments(5 ,50, 50),
+                Arguments.arguments(4 ,50, 50),
+                Arguments.arguments(3 ,50, 50),
+                Arguments.arguments(2 ,50, 50),
+                Arguments.arguments(1 ,50, 50)
+        );
     }
-    
-    @Test
-    void givenPassesWithSellInGreaterThan10_whenUpdate_thenQualityMaxesAt50() {
-        StoreItem pass = StoreItemFactory.createPass(PASSES, 11, 50);
+
+    @ParameterizedTest(name = "#{index} - sellin: {0} startingQuality: {1} expectedQuality: {2}")
+    @MethodSource("createArguments_whenExpired_qualityEquals0")
+    void updateQuality_whenExpired_qualityEquals0(int sellIn, int startingQuality, int expectedQuality) {
+        StoreItem pass = StoreItemFactory.createPass(PASSES, sellIn, startingQuality);
         pass.updateQuality();
-        assertEquals(50, pass.quality);
+        assertThat(pass.quality).isEqualTo(expectedQuality);
     }
-    
-    @Test
-    void givenPassesWithSellInAtOrBetween6And10_whenUpdate_thenQualityMaxesAt50() {
-        StoreItem pass = StoreItemFactory.createPass(PASSES, 10, 50);
-        pass.updateQuality();
-        assertEquals(50, pass.quality);
-    }
-    
-    @Test
-    void givenPassesWithSellInBetween0And6_whenUpdate_thenQualityMaxesAt50() {
-        StoreItem pass = StoreItemFactory.createPass(PASSES, 5, 50);
-        pass.updateQuality();
-        assertEquals(50, pass.quality);
+
+    private static Stream<Arguments> createArguments_whenExpired_qualityEquals0() {
+        return Stream.of(
+                Arguments.arguments(0, 34, 0),
+                Arguments.arguments(-1, 34, 0)
+        );
     }
 }
