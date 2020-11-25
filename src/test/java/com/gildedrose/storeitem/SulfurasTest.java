0ab59b40561338d45ecdf46e927960eecc3f32a8
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2020-11-24 22:42:24 -0600

    refactors tests

--- src/test/java/com/gildedrose/storeitem/SulfurasTest.java
+++ src/test/java/com/gildedrose/storeitem/SulfurasTest.java
@@ -1,10 +1,11 @@
 package com.gildedrose.storeitem;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
 
 public class SulfurasTest {
     private  final String SULFURAS = "Sulfuras, Hand of Ragnaros";
@@ -13,23 +14,18 @@ public class SulfurasTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 0, -1})
-    void givenSulfurasWithPositiveSellIn_whenUpdate_thenQualityDoesNotDecrease(Integer sellIn) {
+    void updateQuality_doesNotChangeQuality(Integer sellIn) {
         StoreItem sulfuras = StoreItemFactory.createSulfuras(SULFURAS, sellIn);
         sulfuras.updateQuality();
         assertEquals(80, sulfuras.quality);
+        assertThat(sulfuras.quality).isEqualTo(80);
     }
 
-    @Test
-    void givenSulfuras_whenUpdate_thenSellInDoesNotDecrement() {
-        StoreItem sulfuras = StoreItemFactory.createSulfuras(SULFURAS, 20);
+    @ParameterizedTest
+    @ValueSource(ints = {1, 0, -1})
+    void updateQuality_doesNotChangeSellIn(int sellIn) {
+        StoreItem sulfuras = StoreItemFactory.createSulfuras(SULFURAS, sellIn);
         sulfuras.updateQuality();
-        assertEquals(20, sulfuras.sellIn);
-    }
-
-    @Test
-    void givenSulfuras_whenUpdate_thenQualityIsAlways80() {
-        StoreItem sulfuras = StoreItemFactory.createSulfuras(SULFURAS, 20);
-        sulfuras.updateQuality();
-        assertEquals(80, sulfuras.quality);
+        assertThat(sulfuras.sellIn).isEqualTo(sellIn);
     }
 }
